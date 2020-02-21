79a130e989a95c7110681429691edba56553a2c7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2020-02-21 23:05:12 +0100

    Add basic login

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const Discord = require('discord.js');
+const Dotenv = require('dotenv');
+Dotenv.config();
+
+const token = process.env.TOKEN;
+
 const bot = new Discord.Client();
 
+bot.on('ready', () => {
+    console.log('Bleep bloop. Marvin is reporting for duty.')
+})
+
+bot.login(token);
