e616940cf93d48dc3a711afcdab0b6ef5bc90df7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2020-02-22 15:52:03 +0100

    Implement dynamic execution of commands

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, meaning_of_life } = require('./config/config.json');
 const Dotenv = require('dotenv');
+
 Dotenv.config();
 
 const token = process.env.TOKEN;
 
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
+// Read available commands from files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // Adds command to commandd collection, with command name as key
+    bot.commands.set(command.name, command);
+}
+
+// Log when started and ready
 bot.on('ready', () => {
     console.log('Bleep bloop. Marvin is reporting for duty. ')
 })
 
 bot.on('message', message => {
+    // Just ignore everything not relevant
+    if (!message.content.startsWith(prefix) || message.author.bot) return;  
     
+    // Prepare message for handling
+	const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!bot.commands.has(commandName)) {
+        if (commandName === meaning_of_life) {
+            message.channel.send('You have found the meaning of life! 🤖');
+            return;
+        } 
+        message.reply('I don\'t know how to do that! 😢');
+    } else {
+        const command = bot.commands.get(commandName);
+
+        try {
+            command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command! 😢');
+        }
+    }
 });
 
 bot.login(token);
