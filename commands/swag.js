b23d53dcb4c49065970d8eff79bd4c0a484b26c2
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2020-05-25 08:37:46 +0200

    Add start and stop + refactor

--- commands/swag.js
+++ commands/swag.js
@@ -5,7 +5,14 @@ module.exports = {
     description: 'Start Discord raffle',
     execute(message, args) {
         if(message.member.roles.find(r => r.name === "admin")) {
-            swag.start(message, args);
+            if (args && args[0] === 'start') {
+                swag.start(message, args);
+            } else if (args && args[0] === 'stop') {
+                swag.stop(message, args);
+            } else {
+                message.channel.send(`Start or stop? You need to tell me. 🤷‍♂️`);
+            }
+            
         } else {
             message.channel.send(`Computer says no.`);
         }
